2b636f905b3d053ab526cc55c54a7013413888d2
Leticia <contatoleticiadia@gmail.com>
2021-10-17 14:42:18 -0300

    feat: Course class created

---
+++ src/Entity/Course.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace MVC\Courses\Entity;
+
+/**
+ * @Entity
+ * @Table(name="courses")
+ */
+class Course
+{
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     */
+    private $id;
+    /**
+     * @Column(type="string")
+     */
+    private $description;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+}
