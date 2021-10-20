c28c1a64c28669d3babfbb365bf00a5e9beffc3e
Leticia <contatoleticiadia@gmail.com>
2021-10-20 18:34:32 -0300

    feat: add isset() and getId() methods

--- view/courses/form.php
+++ view/courses/form.php
@@ -1,8 +1,8 @@
 <?php include __DIR__ . '/../start-html.php'?>
-    <form action="/save-course" method="POST">
+    <form action="/save-course<?= isset($course) ? '?id= ' . $course->getId() : ''; ?>" method="POST">
         <div class="form-group">
             <label for="description">Description</label>
-            <input type="text" name="description" id="description" class="form-control">
+            <input type="text" name="description" id="description" class="form-control" value="<?= isset($course) ? $course->getDescription() : ''; ?>">
         </div>
         <button class="btn btn-primary">Salvar</button>
     </form>
--- view/courses/list-courses.php
+++ view/courses/list-courses.php
@@ -3,8 +3,12 @@
 
     <ul class="list-group">
         <?php foreach ($courses as $course) : ?>
-            <li class="list-group-item">
+            <li class="list-group-item d-flex justify-content-between">
                 <?= $course->getDescription(); ?>
+                <span>
+                    <a href="/change-course?id=<?= $course->getId();?>" class="btn btn-info btn-sm">Changes</a>
+                    <a href="/delete-course?id=<?= $course->getId();?>" class="btn btn-danger btn-sm">Delete</a>
+                </span>
             </li>
         <?php endforeach; ?>
     </ul>
