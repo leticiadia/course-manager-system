6e913f2bba20d103420f13ed7633c54fa7e18b03
Leticia <contatoleticiadia@gmail.com>
2021-10-20 18:22:35 -0300

    feat: add /delete-course and /change-course routes

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,11 @@
 <?php
 
-use MVC\Courses\Controller\InsertIntoForm;
-use MVC\Courses\Controller\ListCourses;
-use MVC\Courses\Controller\Persistence;
+use MVC\Courses\Controller\{Delete, EditForm, InsertIntoForm, ListCourses, Persistence};
 
 return [
     '/courses-list' => ListCourses::class,
     '/new-course' => InsertIntoForm::class,
-    '/save-course' => Persistence::class
+    '/save-course' => Persistence::class,
+    '/delete-course' => Delete::class,
+    '/change-course' => EditForm::class
 ];
