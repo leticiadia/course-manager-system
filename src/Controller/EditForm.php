354a77e68afd676116c32d66d5cc0bba7d9ce985
Leticia <contatoleticiadia@gmail.com>
2021-10-20 18:24:08 -0300

    feat: EditForm class created

---
+++ src/Controller/EditForm.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MVC\Courses\Controller;
+
+use MVC\Courses\Entity\Course;
+use MVC\Courses\Infra\EntityManagerCreator;
+
+class EditForm  extends ControllerWithHTML implements InterfaceControllerRequest
+{
+    private $courseRepository;
+    
+    public function __construct()
+    {
+        $entityManager = (new EntityManagerCreator())->getEntityManager();
+        $this->courseRepository = $entityManager->getRepository(Course::class);
+    }
+
+    public function processRequest(): void
+    {
+        $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+
+        if(is_null($id) || $id === false){
+            header('Location: /change-course');
+            return;
+        }
+
+        $course = $this->courseRepository->find($id);
+        echo $this->renderHTML('courses/form.php', [
+            'course' => $course,
+            'title' => 'Change course: ' . $course->getDescription()
+        ]);
+    }
+}
