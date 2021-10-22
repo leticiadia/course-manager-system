60332c2ea77d37a6090db696c2e3d514d32a31ed
Leticia <contatoleticiadia@gmail.com>
2021-10-22 10:34:47 -0300

    feat: add pattern from PSR-17

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
-use MVC\Courses\Controller\InterfaceControllerRequest;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -20,7 +21,29 @@ if(!isset($_SESSION['logged']) && $loginRoute === false){
     return;
 }
 
+$psr17Factory = new Psr17Factory();
+$creator = new ServerRequestCreator(
+    $psr17Factory, 
+    $psr17Factory, 
+    $psr17Factory, 
+    $psr17Factory 
+); 
+
+$serverRequest = $creator->fromGlobals();
+
 $classController = $routes[$path];
-/** @var InterfaceControllerRequest $controller */
-$controller = new $classController();
-$controller->processRequest();
+
+/** @var ContainerInterface $container */
+$container = require __DIR__ . '/../config/dependicies.php';
+
+/** @var  RequestHandleInterface $controller */
+$controller = $container->get($classController);
+$response = $controller->handle($serverRequest);
+
+foreach ($response->getHeaders() as $name => $values) {
+    foreach ($values as $value) {
+        header(sprintf('%s: %s', $name, $value), false);
+    }
+}
+
+echo $response->getBody();
